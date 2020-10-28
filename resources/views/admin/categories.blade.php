cd5b5a00f105c437b46a8ef00013dfaf6ad44d14
Cong Loi <loitc@paxcreation.com>
2020-10-28 16:32:09 +0700

    Revert "Merge branch 'master' of https://git.heroku.com/glacial-harbor-07970"
    This reverts commit 828c9f44db7701f4cbd6df3788a955bfe053c09a, reversing
    changes made to 8c7a8181bd346a1ce76e8279d2d6d733c597cabf.

---
+++ resources/views/admin/categories.blade.php
@@ -0,0 +1,38 @@
+@extends('admin.layout_admin')
+@section('admin_content')
+<div class="card-body">
+    <div class="table-responsive">
+        <?php
+            $message = Session::get('message_dashboard');
+            if($message) {
+                echo '<span class="text-danger">'.$message.'</span>';
+                Session::put('message_dashboard', null);
+            }
+         ?>
+        <a class="btn btn-success float-right my-3" href="{{URL::to('admin/create-category')}}">Thêm danh mục</a>
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Image</th>
+                    <th>Status</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($categories as $category)
+                <tr>
+                    <td>{{$category->name}}</td>
+                    <td><img src="{{asset('storage/'.$category->image)}}" width="100px" alt=""></td>
+                    <td>{{$category->status}}</td>
+                    <td>
+                    <a class="btn btn-warning" href="{{URL::to('admin/update_categories/'.$category->id)}}">Sửa</a>
+                        <a class="btn btn-danger" onclick="return confirm('Bạn chắc chắn muốn xóa?')" href="{{URL::to('admin/remove_categories/'.$category->id)}}">Xóa</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection
---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Page Title - SB Admin</title>
+        <link href="{{asset('css/styles_admin.css')}}" rel="stylesheet" />
+        <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"
+            crossorigin="anonymous"
+        ></script>
+    </head>
+    <body class="bg-primary">
+        <div id="layoutAuthentication">
+            <div id="layoutAuthentication_content">
+                <main>
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-5">
+                                <div
+                                    class="card shadow-lg border-0 rounded-lg mt-5"
+                                >
+                                    <div class="card-header">
+                                        <h3
+                                            class="text-center font-weight-light my-4"
+                                        >
+                                            Login
+                                        </h3>
+                                    </div>
+                                    <div class="card-body">
+                                        @if (count($errors) >0)
+                                        <ul>
+                                            @foreach($errors->all() as $error)
+                                            <li class="text-danger">
+                                                {{ $error }}
+                                            </li>
+                                            @endforeach
+                                        </ul>
+                                        @endif @if (session('status'))
+                                        <ul>
+                                            <li class="text-danger">
+                                                {{ session('status') }}
+                                            </li>
+                                        </ul>
+                                        @endif
+                                        <form action="{{ route('getLogin') }}" method="post">
+                                            {{ csrf_field() }}
+                                            <div class="form-group">
+                                                <label
+                                                    class="small mb-1"
+                                                    for="inputEmailAddress"
+                                                    >Email</label
+                                                >
+                                                <input
+                                                    class="form-control py-4"
+                                                    id="inputEmailAddress"
+                                                    name="txtEmail"
+                                                    type="email"
+                                                    placeholder="Enter email address"
+                                                />
+                                            </div>
+                                            <div class="form-group">
+                                                <label
+                                                    class="small mb-1"
+                                                    for="inputPassword"
+                                                    >Password</label
+                                                >
+                                                <input
+                                                    class="form-control py-4"
+                                                    id="inputPassword"
+                                                    name="txtPassword"
+                                                    type="password"
+                                                    placeholder="Enter password"
+                                                />
+                                            </div>
+                                            <div
+                                                class="form-group d-flex align-items-center justify-content-between mt-4 mb-0"
+                                            >
+                                                <a
+                                                    class="small"
+                                                    href="password.html"
+                                                    >Forgot Password?</a
+                                                ><input
+                                                    class="btn btn-primary"
+                                                        type="submit"
+                                                        value="Login"
+                                                        name="login"
+                                                />
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        </div>
+        <script
+            src="https://code.jquery.com/jquery-3.5.1.min.js"
+            crossorigin="anonymous"
+        ></script>
+        <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"
+            crossorigin="anonymous"
+        ></script>
+        <script src="{{asset('js/scripts.js')}}"></script>
+    </body>
+</html>
